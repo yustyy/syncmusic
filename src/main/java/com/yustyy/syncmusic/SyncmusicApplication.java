773f8a8bee60a2824179572c94cc666a892218f7
yusssss <yusuf.acmaci@hotmail.com>
2024-05-20 00:03:25 +0300

    initial commit

---
+++ src/main/java/com/yustyy/syncmusic/SyncmusicApplication.java
@@ -0,0 +1,13 @@
+package com.yustyy.syncmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncmusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncmusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yustyy/syncmusic/SyncmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yustyy.syncmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncmusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
